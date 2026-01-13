e93723618a7fe5325e6f7cd1a1f06c00267e7cf5
computerphobe <aryanbharadiyak2006@gmail.com>
2026-01-13 16:52:57 +0530

    Implemented read_files and open_file function

---
+++ tools.py
@@ -0,0 +1,33 @@
+import platform
+import subprocess
+import os
+
+
+def read_files(file_name):
+    try:
+        with open(file_name, 'r', encoding='utf-8') as f:
+            content = f.read(2000)
+            if len(content) == 2000:
+                print("[!] File too long to read showing first 2000 character")
+                print(content)
+
+                user_agreement = input("Do you want to read the rest of file (y/n)")
+                if user_agreement in ["y", "yes"]:
+                    remaining_content = f.read()
+                    print(remaining_content)
+                    return content + remaining_content
+                else:
+                    return content + "__Truncated__"
+            return content
+    except FileNotFoundError:
+        print(f"The requested file not found")
+    except Exception as e:
+        print(f"The following ran occured : {e}")
+
+def open_files(file_name):
+    try:
+        os.startfile(file_name)
+        return f"File Opened: {file_name}"
+    except Exception as e:
+        return f"Error : {e}"
+    
