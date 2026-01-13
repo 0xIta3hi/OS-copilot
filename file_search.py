ec3de876b663062a79854660d9b00915a669990d
computerphobe <aryanbharadiyak2006@gmail.com>
2026-01-13 15:59:41 +0530

    Optimization

--- file_search.py
+++ file_search.py
@@ -7,13 +7,17 @@ def search_files(filename_keyword):
     Skips hidden directories.
     """
     start_path = os.environ('USERPROFILE')
+    search_terms = filename_keyword.lower.split()
     for root, dirs, files in start_path:
-        dirs[:] = [d for d in dirs if not d.startswith('.') and d != "AppData"]
+        dirs[:] = [d for d in dirs if not d.startswith('.') and d != ["AppData", "node_modules", "__pycache__"]]
         for file in files:
             if filename_keyword.lower in file.lower:
                 full_path = os.path.join(root, file)
-                results.append(full_path)
+                full_path_lower = full_path.lower()
+                if all(term in full_path_lower for term in search_terms):
+                    results.append(full_path)
 
-                if len(results) > 5:
-                    return results
+                    if len(results) > 5:
+                        return results
     return results
+
