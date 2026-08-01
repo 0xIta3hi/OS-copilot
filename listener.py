00a6a98103fc0433da7cc112f94092339e303a53
0xIta3hi <aryanbharadiyak2006@gmail.com>
2026-08-01 12:08:58 +0530

    defined tool_registry

--- listener.py
+++ listener.py
@@ -5,4 +5,12 @@ To-Do's:
     - Final output: central LLM chooses the correct tool based on the given input prompt from the user.
 """
 
+TOOL_REGISTRY = {
+    "search_files":"searches for a specific file in the entire OS",
+    "read_file":"Reads the specific file, takes arguments of path and filename.",
+    "open_file":"opens the file.",
+    "send_mail":"Sends a mail via gmail, takes arguments of content, subject, reciepent",
+    "send_discord_message":"sends a discord message to a specific server and channel"
+}
+
 
