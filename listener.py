6dc8cbde9b3e10ff90567ec64b623f6107feacdf
0xIta3hi <aryanbharadiyak2006@gmail.com>
2026-08-01 12:02:14 +0530

    writing the based logic

---
+++ listener.py
@@ -0,0 +1,8 @@
+"""
+To-Do's:
+    - define tool registry. (the dictionary which contains all the tools and their descriptions in a key value pair.)
+    - write the logic for central llm agent to select a tool from the registry and output that. 
+    - Final output: central LLM chooses the correct tool based on the given input prompt from the user.
+"""
+
+
