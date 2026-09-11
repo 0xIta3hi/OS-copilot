4003871492886f43e77ee984d9bf4d561b13e497
0xIta3hi <aryanbharadiyak2006@gmail.com>
2026-09-11 08:59:41 +0530

    implemented local file system class methods

--- plugins/local_files.py
+++ plugins/local_files.py
@@ -1,30 +1,15 @@
 import os
 
+class Local_File_system:
+    def __init__(self):
+        pass
 
-def read_files(file_name):
-    try:
-        with open(file_name, 'r', encoding='utf-8') as f:
-            content = f.read(10000)
-            if len(content) == 10000:
-                print("[!] File too long to read showing first 10000 character")
-                print(content)
+    def file_search(path):
+        pass
 
-                user_agreement = input("Do you want to read the rest of file (y/n)")
-                if user_agreement in ["y", "yes"]:
-                    remaining_content = f.read()
-                    print(remaining_content)
-                    return content + remaining_content
-                else:
-                    return content + "__Truncated__"
-            return content
-    except FileNotFoundError:
-        print(f"The requested file not found")
-    except Exception as e:
-        print(f"The following ran occured : {e}")
+    def file_system_manipulation(action, path):
+        pass
 
-def open_files(file_name):
-    try:
-        os.startfile(file_name)
-        return f"File Opened: {file_name}"
-    except Exception as e:
-        return f"Error : {e}"
+    def open_file(filename, path):
+        pass
+
